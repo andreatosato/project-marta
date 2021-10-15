a5eea7a0ebbe742b1436047657153a735ce5e3c7
Andrea Tosato <andrea.tosato@hotmail.it>
2021-10-15 13:34:13 +0200

    Permission Microphone

---
+++ src/Client/ProjectMarta/Services/MicrophoneService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace ProjectMarta.Services
+{
+    class MicrophoneService : IMicrophoneService
+    {
+        public async Task<bool> GetPermissionAsync()
+        {
+            var microphonePermission = await Permissions.CheckStatusAsync<Permissions.Microphone>();
+            if (microphonePermission != PermissionStatus.Granted)
+            {
+                microphonePermission = await Permissions.RequestAsync<Permissions.Microphone>();
+            }
+
+            return microphonePermission == PermissionStatus.Granted;
+        }
+
+        public void OnRequestPermissionResult(bool isGranted)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
